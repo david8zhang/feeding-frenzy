0e9b6a6470563cd73545a5a40b4b700e3a49e1cf
David Zhang <david8zhang@berkeley.edu>
2023-07-19 22:36:23 -0700

    initial commit

---
+++ scripts/Player.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@export var speed = 480
+var screen_size
+
+signal hit
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$Sprite2D.flip_h = velocity.x < 0
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
